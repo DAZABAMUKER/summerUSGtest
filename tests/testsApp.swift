96f840329b1c6e5ffb37c0bee7424bb8db53ec79
DAZABAMUKER <wookis112@gmail.com>
2023-04-20 23:42:31 +0900

    Initial Commit

---
+++ tests/testsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testsApp.swift
+//  tests
+//
+//  Created by 안병욱 on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct testsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
